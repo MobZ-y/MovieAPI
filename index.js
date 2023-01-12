514edce6653f182fe3883fa69a27ed9b04c951b4
unknown <romain.nourrydu66@gmail.com>
2023-01-13 00:18:50 +0100

    search done

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const tren = document.getElementById('trending');
 const content = document.querySelector(".content");
+const btn = document.querySelector("button")
+const topMovie = document.querySelector('.moviesTOP')
+const inputSearch = document.getElementById("inputSearch")
+
+
 let trending = [];
-let peoplesearch = "Ana de Armas";
+let peoplesearch = "";
 
 
 async function FetchPeople() {
@@ -10,28 +15,42 @@ async function FetchPeople() {
     .then((data) =>  peoplesearch = data);
 
     console.log(peoplesearch);
-    FetchDisplayPeople()
 }
 
-const inputSearch = document.getElementById("inputSearch")
+
 
 function FetchDisplayPeople() {
-    content.innerHTML = peoplesearch.filter((country) =>
-    country.results[0].name
-      .toLowerCase()
-      .includes(inputSearch.value.toLowerCase())
-  )
     content.innerHTML = peoplesearch = 
         `
-        <div class="card">
+        <div class="search">
+        <div class="Profile">
+        <img src="https://image.tmdb.org/t/p/w500${peoplesearch.results[0].profile_path
+    }" alt="drapeau" id="pp"> 
+    </div>
+        <div class="KnowFor">
         <h3>${peoplesearch.results[0].name}</h3>
+        <h4>Connu pour :</h4>
+        <img src="https://image.tmdb.org/t/p/w500${peoplesearch.results[0].known_for[0].poster_path
+      }" alt="poster" >
+      <img src="https://image.tmdb.org/t/p/w500${peoplesearch.results[0].known_for[1].poster_path
+    }" alt="poster" >
+    <img src="https://image.tmdb.org/t/p/w500${peoplesearch.results[0].known_for[2].poster_path
+  }" alt="poster" >
+        </div>
         </div>
       `
-    
+};
 
-}
+inputSearch.addEventListener('input', (e) => {
+  peoplesearch = e.target.value
+  FetchPeople()
+});
 
-inputSearch.addEventListener("input",  FetchDisplayPeople);
+btn.addEventListener("click", ()=>{
+  topMovie.classList.add("visiblity");
+  FetchDisplayPeople()
+})
+
 
 async function FetchTREN() {
     await fetch('https://api.themoviedb.org/3/trending/all/day?api_key=dc4fa11dbb0888468121f0e93ac98077&page=1')
@@ -40,7 +59,6 @@ async function FetchTREN() {
 
 
     FetchDisplay()
-
 }
 
 
@@ -51,7 +69,7 @@ tren.innerHTML = trending.map((info) =>
     <img src="https://image.tmdb.org/t/p/w500${info.poster_path
     }" alt="drapeau" > 
     <h2>${info.title === undefined ? info.name : info.title}</h2>
-    <h3>Note:${info.vote_average}</h3>
+    <h3>Note:${info.vote_average === 0 ? "unrated " : info.vote_average}</h3>
     </div>
   `
 ).join("")

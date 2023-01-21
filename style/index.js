bcdd04b916106e2845cc95cd21efe1d867603d9a
unknown <romain.nourrydu66@gmail.com>
2023-01-22 00:46:17 +0100

    add person

--- style/index.js
+++ style/index.js
@@ -5,6 +5,10 @@ const topMovie = document.querySelector(".movies-trending");
 const inputSearch = document.getElementById("inputSearch");
 const TVswitch = document.getElementById("TV");
 const TendancesSwitch = document.getElementById("Tend");
+const TendancesPerson = document.querySelector(".trendingPerson");
+const DaySwitch = document.getElementById("Day");
+const WeekSwitch = document.getElementById("Week");
+
 
 let trending = [];
 let trendingTv = [];
@@ -12,7 +16,8 @@ let peoplesearch = "Ana de Armas";
 let idPeople = "";
 let People = "";
 let date1 = new Date();
-console.log(date1);
+let SwitchPerson = "week";
+let TrendingPerson = [];
 
 async function FetchPeople() {
   await fetch(
@@ -120,7 +125,7 @@ async function FetchTV() {
   // FetchDisplayTv();
 }
 
-window.addEventListener("load", FetchTREN(), FetchPeople(), FetchTV());
+window.addEventListener("load", FetchTREN(), FetchPeople(), FetchTV(), FetchDiscovery());
 
 TVswitch.addEventListener("click", (e) => {
   e.preventDefault();
@@ -132,4 +137,54 @@ Tend.addEventListener("click", (e) => {
   e.preventDefault();
   trendingTV = trending;
   FetchTREN();
+});
+//!******************************************************************* DISCOVERY
+async function FetchDiscovery() {
+  await fetch(
+    "https://api.themoviedb.org/3/trending/person/" +SwitchPerson+ "?api_key=dc4fa11dbb0888468121f0e93ac98077"
+  )
+    .then((res) => res.json())
+    .then((data) => trendingPerson = data.results);
+
+    console.log(trendingPerson)
+
+    DisplayDiscovery()
+
+}
+
+function DisplayDiscovery() {
+  TendancesPerson.innerHTML = trendingPerson
+    .map(
+      (info) =>
+        `
+    <div class="card">
+    <div class="card-popular">
+    <div class="profile-popular">
+    <img src="https://image.tmdb.org/t/p/w500${
+      info.profile_path
+
+    }" alt="photo de ${info.name}" > 
+</div>
+<div class="profile-meta">
+    <p class="name">${info.name === undefined ? info.title : info.name}</p>
+</div>
+    </div>
+    </div>
+    </div>
+  `
+    )
+    .join("");
+};
+
+DaySwitch.addEventListener('click', (e) =>{
+  e.preventDefault();
+  SwitchPerson  = "day";
+  FetchDiscovery()
+});
+
+
+WeekSwitch.addEventListener('click', (e) =>{
+  e.preventDefault();
+  SwitchPerson  = "week";
+  FetchDiscovery()
 });

31ec9961b5c99f9a6f2dd37529900c48ea6efc29
unknown <romain.nourrydu66@gmail.com>
2023-01-23 02:26:57 +0100

    q

--- style/search.js
+++ style/search.js
@@ -3,10 +3,12 @@ const filterContainer = document.querySelector(".filter-container")
 const topMovie = document.querySelector(".movies-trending");
 const topPerson = document.querySelector(".trendingOnPerson");
 const inputSearch = document.getElementById("inputSearch");
+const Credits = document.querySelector(".Credits");
 
 let peoplesearch = "Ana de Armas";
 let idPeople = "";
 let People = "";
+let Credit = [];
 
 async function FetchPeople() {
   await fetch(
@@ -38,6 +40,7 @@ function FetchDisplayPeople() {
 
 
   content.innerHTML = People = `
+  <div class="search-wrap">
           <div class="search">
           <div class="Profile">
           <img src="https://image.tmdb.org/t/p/w500${People.profile_path}" alt="drapeau" id="pp"> 
@@ -49,12 +52,18 @@ function FetchDisplayPeople() {
           <p>${People.biography}</p>
           </div>
           </div>
+          </div>
         `;
 }
+
+
+
+
 
 inputSearch.addEventListener("input", (e) => {
   peoplesearch = e.target.value;
   FetchPeople();
+  FetchCreditMovies()
 });
 
 btn.addEventListener("click", () => {
@@ -65,4 +74,48 @@ btn.addEventListener("click", () => {
   FetchDisplayPeople();
 });
 
-window.addEventListener("load", FetchPeople());
+window.addEventListener("load", FetchPeople(),);
+
+
+//*********************Movie Credit */
+
+async function FetchCreditMovies() {
+  await fetch(
+    "https://api.themoviedb.org/3/person/224513/movie_credits?api_key=dc4fa11dbb0888468121f0e93ac98077&language=en-US"
+  )
+    .then((res) => res.json())
+    .then((data) =>  Credit = data.cast);
+
+console.log(Credit)
+    DisplayCarousel()
+ 
+}
+
+function DisplayCarousel() {
+  Credits.innerHTML =Credit.map(
+    (info) =>
+      `
+  <div class="card">
+  <div class="card-popular">
+  <div class="profile-popular">
+  <img src="https://image.tmdb.org/t/p/w500${
+    info.poster_path
+  }" alt="photo de ${info.name}" > 
+</div>
+<div class="profile-meta">
+  <p class="name">${info.name === undefined ? info.title : info.name}</p>
+  <div id="canvas" class="${
+    info.vote_average === 0 ? "" : info.vote_average > 6 ? "green" : "red"
+  }">${
+        info.vote_average === 0
+          ? "unrated"
+          : Math.floor(info.vote_average * 10).toFixed(0)
+      }</div>
+</div>
+  </div>
+  </div>
+  </div>
+`
+  )
+  .join("");
+}
